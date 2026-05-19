5ec61cfb6e92d81b07f64bb20e97fd2e0439da37
MKN-Sai-Varun <mknsvarun@gmail.com>
2026-05-19 21:09:08 +0530

    Updated CORS Permissions

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ app.use(helmet({
 
 // CORS middleware
 app.use(cors({
-  origin: "http://localhost:3001", // Next.js dev server port
+  origin: process.env.FRONTEND_URL || "http://localhost:3001",
   credentials: true
 }));
 
